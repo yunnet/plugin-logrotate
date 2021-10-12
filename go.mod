295052a2e3af721ccc949ac16ff80fc4e5506a5e
yunnet <yunnet@gmail.com>
2021-10-12 12:36:47 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/yunnet/plugin-logrotate/v3
+
+go 1.17
+
+require github.com/yunnet/engine v1.0.4
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,165 @@
+package logrotate
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	. "github.com/yunnet/engine"
+	//. "github.com/yunnet/utils/v3"
+)
+
+var config LogRotate
+
+type FileInfo struct {
+	Name string
+	Size int64
+}
+type LogRotate struct {
+	Path        string
+	Size        int64
+	Days        int
+	file        *os.File
+	currentSize int64
+	createTime  time.Time
+	hours       float64
+	splitFunc   func() bool
+	Formatter   string
+}
+
+func init() {
+	InstallPlugin(&PluginConfig{
+		Name:   "LogRotate",
+		Config: &config,
+		Run:    run,
+	})
+}
+func run() {
+	http.HandleFunc("/api/logrotate/tail", watchLogs)
+	http.HandleFunc("/api/logrotate/find", findLog)
+	http.HandleFunc("/api/logrotate/list", listLogFiles)
+	http.HandleFunc("/api/logrotate/open", openLog)
+	http.HandleFunc("/api/logrotate/download", download)
+	if config.Size > 0 {
+		config.splitFunc = config.splitBySize
+	} else {
+		if config.Days == 0 {
+			config.Days = 1
+		}
+		config.hours = float64(config.Days) * 24
+		config.splitFunc = config.splitByTime
+	}
+	config.createTime = time.Now()
+	if config.Formatter == "" {
+        	config.Formatter = "2006-01-02T15"
+	}
+	err := os.MkdirAll(config.Path, 0777)
+	config.file, err = os.OpenFile(filepath.Join(config.Path, fmt.Sprintf("%s.log", config.createTime.Format(config.Formatter))), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err == nil {
+		stat, _ := config.file.Stat()
+		config.currentSize = stat.Size()
+		AddWriter(&config)
+	} else {
+		log.Println(err)
+	}
+}
+func (l *LogRotate) splitBySize() bool {
+	return l.currentSize >= l.Size
+}
+func (l *LogRotate) splitByTime() bool {
+	return time.Since(l.createTime).Hours() > l.hours
+}
+func (l *LogRotate) Write(data []byte) (n int, err error) {
+	n, err = l.file.Write(data)
+	l.currentSize += int64(n)
+	if err == nil {
+		if l.splitFunc() {
+			l.createTime = time.Now()
+			if file, err := os.OpenFile(filepath.Join(l.Path, fmt.Sprintf("%s.log", l.createTime.Format(config.Formatter))), os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0666); err == nil {
+				l.file = file
+				l.currentSize = 0
+			}
+		}
+	}
+	return
+}
+func watchLogs(w http.ResponseWriter, r *http.Request) {
+	AddWriter(NewSSE(w, r.Context()))
+	<-r.Context().Done()
+}
+func findLog(w http.ResponseWriter, r *http.Request) {
+	cmd := exec.Command("grep", fmt.Sprintf("\"%s\"", r.URL.Query().Get("query")), config.Path)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Run()
+	w.Write([]byte(out.String()))
+}
+func listLogFiles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	dir, err := os.Open(config.Path)
+	defer func() {
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+	}()
+	if err != nil {
+		return
+	}
+	var files []os.FileInfo
+	files, err = dir.Readdir(0)
+	if err != nil {
+		return
+	}
+	var fileInfos []*FileInfo
+	for _, info := range files {
+		fileInfos = append(fileInfos, &FileInfo{
+			info.Name(), info.Size(),
+		})
+	}
+	var bytes []byte
+	bytes, err = json.Marshal(fileInfos)
+	if err != nil {
+		return
+	}
+	w.Write(bytes)
+}
+func download(w http.ResponseWriter, r *http.Request) {
+	filename := r.URL.Query().Get("file")
+	file, err := os.Open(filepath.Join(config.Path, filename))
+	defer func() {
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+	}()
+	w.Header().Add("Content-Disposition", "attachment; filename="+filename)
+	if err != nil {
+		return
+	}
+	_, err = io.Copy(w, file)
+	if err != nil {
+		return
+	}
+}
+func openLog(w http.ResponseWriter, r *http.Request) {
+	filename := r.URL.Query().Get("file")
+	file, err := os.Open(filepath.Join(config.Path, filename))
+	defer func() {
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+	}()
+	if err != nil {
+		return
+	}
+	_, err = io.Copy(w, file)
+	if err != nil {
+		return
+	}
+}
